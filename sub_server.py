9648a20d9a53781c0ef0828cd48278e6196dee2c
yuras <yura.shubnyj@mail.ru>
2022-10-07 01:15:12 +0300

    ETH 2.0 fixed disconnect

--- sub_server.py
+++ sub_server.py
@@ -1,46 +1,55 @@
 import websockets
 import asyncio
 import funcs
-import threading
 
 PORT = 5000
 
 print("Started server and its listening on Port " + str(PORT))
 
+connected = {}
+
 
 async def echo(websocket):
     print("A client just connected")
-    try:
-        message = await websocket.recv()
-        print("Received message from client: " + message)
-        user_wallet, addr, coin_g = funcs.reform_getting_data(message)
-        if len(addr) != 0:
-            funcs.add_user_to_db(user_wallet)
-            coin = funcs.up_reggister(coin_g)
-            balance = funcs.get_balance_ETH(addr)
-            response = funcs.response_form(coin, addr, balance)
-            funcs.add_user_sub(user_wallet, addr, coin, balance)
-            if funcs.check_same_users_subs(user_wallet, addr) == 1:
-                await websocket.send("You have subscribed to: " + response)
+    connected[websocket] = None
+    while True:
+        try:
+            message = await websocket.recv()
+            print("Received message from client: " + message)
+            user_wallet, addr, coin_g = funcs.reform_getting_data(message)
+            connected[websocket] = user_wallet
+            print(connected)
+            if len(addr) != 0:
+                funcs.add_user_to_db(user_wallet)
+                coin = funcs.up_reggister(coin_g)
+                balance = funcs.get_balance_ETH(addr)
+                response = funcs.response_form(coin, addr, balance)
+                funcs.add_user_sub(user_wallet, addr, coin, balance)
+                if funcs.check_same_users_subs(user_wallet, addr) == 1:
+                    await websocket.send("You have subscribed to: " + response)
+                else:
+                    await websocket.send("You have already subscribed to: " + response)
+
+                while True:
+                    send_info = funcs.update_balances(user_wallet)
+                    if send_info != 'error' and send_info != None:
+                        for msg in send_info:
+                            await websocket.send(msg)
+                        await asyncio.sleep(120)
             else:
-                await websocket.send("You have already subscribed to: " + response)
-
-            while True:
-                send_info = funcs.update_balances(user_wallet)
-                if send_info != 'error' and send_info != None:
-                    for msg in send_info:
-                        await websocket.send(msg)
-                    await asyncio.sleep(120)
-        else:
-            subs = funcs.show_all_subs(user_wallet)
-            await websocket.send(subs)
+                subs = funcs.show_all_subs(user_wallet)
+                await websocket.send(subs)
 
 
-    except websockets.ConnectionClosed:
-        print("A client just disconnected")
+        except websockets.ConnectionClosed:
+            print(f"A client {connected[websocket]} just disconnected")
+            del connected[websocket]
+            print(connected)
 
 
-start_server = websockets.serve(echo, "localhost", PORT)
+async def main():
+    async with websockets.serve(echo, "localhost", PORT):
+        await asyncio.Future()
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+
+asyncio.run(main())
